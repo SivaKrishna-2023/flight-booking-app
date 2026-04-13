bf8168c682cd7d4199da586d1ac83f10ff77ba45
SivaKrishna-2023 <sivakrishnagoriparthi520@gmail.com>
2026-04-13 19:03:48 +0530

    Added airport resource CURD

---
+++ src/middlewares/airport-middlewares.js
@@ -0,0 +1,35 @@
+const { StatusCodes } = require("http-status-codes");
+const { ErrorResponse } = require("../utils/common");
+const { AppError } = require("../utils/errors/app-error");
+
+function validateCreateRequest(req, res, next) {
+  if (!req.body.name) {
+    ErrorResponse.message = "something went wrong while creating airport";
+    ErrorResponse.error = new AppError(
+      ["Name not found in the incomming request in the current form"],
+      StatusCodes.BAD_REQUEST,
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+
+  if (!req.body.code) {
+    ErrorResponse.message = "something went wrong while creating airport";
+    ErrorResponse.error = new AppError(
+      ["Code not found in the incomming request in the current form"],
+      StatusCodes.BAD_REQUEST,
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+
+  if (!req.body.cityId) {
+    ErrorResponse.message = "something went wrong while creating airport";
+    ErrorResponse.error = new AppError(
+      ["City id not found in the incomming request in the current form"],
+      StatusCodes.BAD_REQUEST,
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  next();
+}
+
+module.exports = { validateCreateRequest };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const airplaneRoutes = require("./airplane-routes");
+const cityRoutes = require("./city-routes");
+const airportRoutes = require("./airport-routes");
 
-const airplaneRoutes = require("./airplane-routes")
-const cityRoutes = require("./city-routes")
+const router = express.Router();
 
-const router = express.Router()
+router.use("/airplanes", airplaneRoutes);
+router.use("/cities", cityRoutes);
+router.use("/airports", airportRoutes);
 
-router.use("/airplanes", airplaneRoutes)
-router.use("/cities", cityRoutes)
-
-module.exports = router
+module.exports = router;
